c84cef944fd20444f959c281705bf953975dae7d
siri lakshmi <sirilakshmi1703@gmail.com>
2021-08-17 18:46:49 +0530

    Area with two positive Integers

--- src/main/java/RectangleAreaPerimeter.java
+++ src/main/java/RectangleAreaPerimeter.java
@@ -1,2 +1,11 @@
 public class RectangleAreaPerimeter {
+    private int l,b;
+    public RectangleAreaPerimeter(int l, int b) {
+        this.l=l;
+        this.b=b;
+    }
+
+    public int area() {
+        return l*b;
+    }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,6 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectangleTest {
+    @Test
+    public void toCheckAreaisSixwhenIntegersAre3and2()
+    {
+        RectangleAreaPerimeter rectangle=new RectangleAreaPerimeter(3,2);
+        int actualvalue=rectangle.area();
+        assertEquals(6,actualvalue);
+    }
+
 
 
 }
+
