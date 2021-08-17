826ac0e53657a73aa08467136be0aef26a1c28e8
siri lakshmi <sirilakshmi1703@gmail.com>
2021-08-17 18:55:02 +0530

    Area with Two Negative Integers

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RectangleTest {
+
     @Test
     public void toCheckAreaisSixwhenIntegersAre3and2()
     {
@@ -10,6 +11,15 @@ public class RectangleTest {
         int actualvalue=rectangle.area();
         assertEquals(6,actualvalue);
     }
+
+    @Test
+    public void toCheckAreais8WhenIntegersAreMinus4andMinus2()
+    {
+        RectangleAreaPerimeter rectangle=new RectangleAreaPerimeter(-4,-2);
+        int actualvalue=rectangle.area();
+        assertEquals(8,actualvalue);
+    }
+
 
 
 
